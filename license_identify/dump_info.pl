8efc6c9a187ddf28722156a5321fb97ed3b442f9
Ji Xiang <jidiablo@aliyun.com>
2016-04-18 19:34:55 +0800

    remove file of useless

--- license_identify/dump_info.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-use File::Copy;
-use Storable;
-use Data::Dumper;
-
-$serialized_file = '/home/xji/Source_Code_Audit/serizlized_rpm_to_srcrpm.lorui';
-
-my $retrieved_rpm_mapping = retrieve($serialized_file);
-print Dumper($retrieved_rpm_mapping);
