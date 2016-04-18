e9998acb4383394d6512cb36a18ba0e84456c83f
Ji Xiang <jidiablo@aliyun.com>
2016-04-18 18:48:27 +0800

    add more file list into repository

---
+++ file_list/down_source.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+wget --no-check-certificate  --user=xiang_ji_dev --password=syberos  -P. --mirror --no-parent -r -N -nH -l inf --cut-dirs=4 -A src.rpm https://xiang_ji_dev:el%26%5f67kU6@repo2.insyber.com/syberos:/devel:/base/standard_armv7tnhl/src/
+
+wget --no-check-certificate  --user=xiang_ji_dev --password=syberos  -P. --mirror --no-parent -r -N -nH -l inf --cut-dirs=4 -A src.rpm https://xiang_ji_dev:el%26%5f67kU6@repo2.insyber.com/syberos:/devel:/nonbase/standard_armv7tnhl/src/
+
+wget --no-check-certificate  --user=xiang_ji_dev --password=syberos  -P. --mirror --no-parent -r -N -nH -l inf --cut-dirs=4 -A src.rpm https://xiang_ji_dev:el%26%5f67kU6@repo2.insyber.com/syberos:/devel:/qt/standard_armv7tnhl/src/
+
+wget --no-check-certificate  --user=xiang_ji_dev --password=syberos  -P. --mirror --no-parent -r -N -nH -l inf --cut-dirs=4 -A src.rpm https://xiang_ji_dev:el%26%5f67kU6@repo2.insyber.com/syberos:/devel:/mw/standard_armv7tnhl/src/
+
+
+wget --no-check-certificate  --user=xiang_ji_dev --password=syberos  -P. --mirror --no-parent -r -N -nH -l inf --cut-dirs=4 -A src.rpm https://xiang_ji_dev:el%26%5f67kU6@repo2.insyber.com/syberos:/devel:/soservice/standard_armv7tnhl/src/
+
+wget --no-check-certificate  --user=xiang_ji_dev --password=syberos  -P. --mirror --no-parent -r -N -nH -l inf --cut-dirs=4 -A src.rpm https://xiang_ji_dev:el%26%5f67kU6@repo2.insyber.com/syberos:/devel:/applications/standard_armv7tnhl/src/
+
+wget --no-check-certificate  --user=xiang_ji_dev --password=syberos  -P. --mirror --no-parent -r -N -nH -l inf --cut-dirs=4 -A src.rpm https://xiang_ji_dev:el%26%5f67kU6@repo2.insyber.com/syberos:/devel:/hw/standard_armv7tnhl/src/
+
+wget --no-check-certificate  --user=xiang_ji_dev --password=syberos  -P. --mirror --no-parent -r -N -nH -l inf --cut-dirs=4 -A src.rpm https://xiang_ji_dev:el%26%5f67kU6@repo2.insyber.com/syberos:/devel:/hw:/spreadtrum/standard_armv7tnhl/src/
+
+
+wget --no-check-certificate  --user=xiang_ji_dev --password=syberos  -P. --mirror --no-parent -r -N -nH -l inf --cut-dirs=4 -A src.rpm https://xiang_ji_dev:el%26%5f67kU6@repo2.insyber.com/syberos:/devel:/hw:/spreadtrum:/cactus/standard_armv7tnhl/src/
